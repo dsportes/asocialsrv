3bc146f4b046c6a59c67b3eeafde5edf104e4268
daniel <daniel@NUC>
2018-08-02 19:50:25 +0200

    test getHome

--- asocialsrv/test.py
+++ asocialsrv/test.py
@@ -1,6 +1,17 @@
 import os, json
 
-def listRes(pyp, build, shortcuts, cp = None):
+def getHome(pyp, build, home):
+    lst = []
+    with open(pyp + "/build/" + build + "/" + home + ".html", "r") as ins:
+        for line in ins:
+            i = line.find("<base href=")
+            if i != -1:
+                lst.append("<base href=\"./build/" + build + "/\">\n")
+            else:
+                lst.append(line)
+    return "".join(lst)
+
+def getSwjs(pyp, build, shortcuts, cp = None):
     rootdir = pyp + "/build/" + build
     lx = len(rootdir)
     cx = "/" + cp + "/$ui" if (cp is not None and len(cp) != 0) else "/$ui"
@@ -12,12 +23,19 @@ def listRes(pyp, build, shortcuts, cp = None):
             lst.append(cx + filepath[lx:])
     d1 = "const shortcuts = " + json.dumps(shortcuts) + ";\n"
     d2 = "const build = \"" + build + "\";\nconst cp = \"" + ("" if cp is None else cp) + "\";\nconst lres = [\n"
-    f = open("ui/" + build + "/src/sw.js", "r")
+    f = open("build/" + build + "/src/sw.js", "r")
     t = f.read();
     return d1 + d2 + ",\n".join(lst) + "\n];\n" + t
 
 shortcuts = {"a":"prod-index", "d":"demo-home"}
 pyp = os.path.dirname(__file__)
 build = "1.1"
+cp = "cp"
+home = "index"
 
-print(listRes(pyp, build, shortcuts, "cp"))
+print(getSwjs(pyp, build, shortcuts, cp))
+
+print("______________________________________________")
+
+print(getHome(pyp, build, home))
+
