93309ffaec1d27c8e9b7b0d6be6e52eb41fc764d
daniel <daniel@NUC>
2018-08-04 20:11:00 +0200

    generation home ok

--- asocialsrv/uiwsgi.py
+++ asocialsrv/uiwsgi.py
@@ -13,100 +13,41 @@ https://site/cpui/ping
 
 ############## Classe Configuration
 class Cfg:
-    def __init__(self, pyp):
+    def __init__(self):
         # context-path UI
-        self.cpui = "ui"
+        self.cp = "cp"
+        
         # niveau d'intrerface
         self.inb = 1
-        # builds UI servies, la première est celle conseillée (une seule obligatoire)
+        
+        # builds UI servies, la première est l'officielle (une seule obligatoire)
         self.uib = [1, 2]
-        # builds OP servies, la première est celle conseillée (une seule obligatoire)
-        self.opb = [1, 2]
-        # path des ressources chez l'hébergeur. None si c'est dans le répertoire sous celui de ce script
-        self.respath = "/home/daniel/git/asocial/asocial/build"
         
-        # URL des sites pour chaque organisation
-        d1 = "http://localhost:8000/op"
-        self.sites = {"org1":d1, "org2":d1}
-        
-        # templates applicables à chaque page d'accueil
-        t1 = "home1.html"
-        t2 = "home2.html"
-        self.homes = {"adh":t1, "admin":t2}
-        
-        # page d'accueil par défaut
-        self.defhome = "adh"
-        
+        # URL des serveurs op pour chaque organisation
+        d1 = "http://localhost:8000/cp/$op"
+        self.opsites = {"prod":d1, "demo":d1}
+                
         # raccourcis
-        self.shortcuts = {"?":"org1/adh", "ad":"org1/admin"}
+        self.homeShortcuts = {"?":"prod-index", "index2":"prod-index2", "index":"prod-index", "d":"demo-index", "admin":"prod-index2"}
         
         # langues supportées, la première est celle par défaut (obligatoire)
         self.langs = ["fr", "en"]
-        
-        if self.respath is not None:
-            self.respath = pyp
-        if self.respath[-1] != "/":
-            self.respath += "/"
-
-    def defLg(self, lgx = None):
-        return lgx if (lgx is not None or lgx in self.langs) else self.langs[0]
-    def shorcut(self, sc = None):
-        return self.shortcuts["?"] if sc is None else self.shortcuts[sc]
-    def defUib(self):
-        return str(self.inb) + "." + str(self.uib[0])
-        
 ############### Fin de configuration
 
-import os, sys
-from wsgiref.util import request_uri, application_uri
-from wsgiref.simple_server import make_server
+cfg = Cfg()
 
-pyp = os.path.dirname(__file__)
-sys.path.insert(0, pyp)
-
-cfg = Cfg(pyp)
-
-class Dic:
-    def __init__(self, cfg):
-        Dic.d1 = " - 0:{0} 1:{1} 2:{2} 3:{3}"
-        self.cfg = cfg;
-        self.dic = {}
-        for x in cfg.langs:
-            self.dic[x] = {}
-        self.set("fr", "mf", "L'adresse de la page est mal formée : ")
-        self.set("fr", "mode", "les seules extensions auttorisées sont .i (mode incognito) et .a (mode avion). [{0}] a été trouvé")
-        self.set("fr", "build", "le code d'une build doit être de la forme 4.7 ou 4.7.5 ([{0}] a été trouvé)")
-        self.set("fr", "majeur", "seules les builds de niveau {0}. sont déployées. {1}. a été trouvé")
-        self.set("fr", "mineur", "seules les builds {0}. sont déployées. {1} a été trouvé")
-            
-    def set(self, lg, code, msg):
-        if code is None or msg is None:
-            return
-        l = self.cfg.defLg(lg)
-        self.dic[l][code] = msg
-    def get(self, lg, code):
-        if code is None:
-            return "?" + Dic.d1
-        m = self.dic[self.cfg.defLg(lg)][code]
-        return m if m is not None else code + Dic.d1
-    def deplUib(self):
-        s = 0
-        for x in self.uib:
-            s += str(self.inb) + "." + str(x) + " "
-        return s
-    def deplOpb(self):
-        return self.opb.join(" ")
-
-dic = Dic(cfg)
+def defLang(lgx = None):
+    global cfg
+    return lgx if (lgx is not None or lgx in cfg.langs) else cfg.langs[0]
 
 class AL:
-    AL.LEVEL = 2
+    LEVEL = 2
     def setInfo():
         AL.LEVEL = 0
     def setWarn():
-        LEVEL = 0
+        AL.LEVEL = 0
     def setError():
-        LEVEL = 0
+        AL.LEVEL = 0
     def info(text):
         if AL.LEVEL >= 0:
             print(text, file=sys.stderr)
@@ -116,116 +57,203 @@ class AL:
     def error(text):
         print(text, file=sys.stderr)
 
+class Dic:
+    def __init__(self):
+        global cfg;
+        Dic.d1 = " - 0:{0} 1:{1} 2:{2} 3:{3}"
+        self.dic = {}
+        for x in cfg.langs:
+            self.dic[x] = {}
+        self.set("fr", "rb", "Le répertoire de la build [{0}] n'est pas accessible : [{1}]")
+        self.set("fr", "home", "la page d'accueil [{0}] pour la build [{1}] (path:[{2}]) n'est pas accessible")
+        self.set("fr", "majeur", "la ou les builds déployées sont {0}.x : une {1}.x a été demandée")
+        self.set("fr", "mineur", "la ou les builds déployées sont {0}.{1} : {2} a été demandée")
+        self.set("fr", "org", "l'organisation {0} n'est pas hébergée")
+            
+    def set(self, lang, code, msg):
+        if code is None or msg is None:
+            return
+        l = defLang(lang)
+        self.dic[l][code] = msg
+        
+    def get(self, lang, code):
+        if code is None:
+            return "?" + Dic.d1
+        lx = defLang(lang)
+        m = self.dic[lx].get(code, None)
+        if m is None and lx != cfg.langs[0]:
+            m = self.dic[cfg.langs[0]].get(code, None)
+        return m if m is not None else code + Dic.d1
+
+dic = Dic()
+
+def homeShortcut(sc = None):
+    global cfg
+    return cfg.homeShortcuts["?"] if sc is None else cfg.homeShortcuts.get(sc, None)
+
+import os, sys, json
+from wsgiref.simple_server import make_server
+
+pyp = os.path.dirname(__file__)
+sys.path.insert(0, pyp)
+
 class AppEx(Exception):
-    def __init__(self, lg, code, args = []):
+    def __init__(self, lang, code, args = []):
         global dic
-        self.msg = dic.get(lg, code)
+        self.msg = dic.get(lang, code)
         Exception.__init__(self, self.msg.format(code, args))
 
-class UiUrl:
+class Url:
     def __init__(self, environ):
         global cfg
-        self.pi = environ["PATH_INFO"]
-        self.qs = environ["QUERY_STRING"]
-        self.lg = cfg.defLg()
-        if self.qs is not None:
-            i = self.qs.find("lg=")
-            if i != -1:
-                j = self.qs.index("&", i + 3)
-                self.lg = self.qs[i:j] if j != -1 else self.qs[i:]
-            else:
-                self.lg = cfg.defLg()
-        self.lg = cfg.defLg(self.lg)
+        p = environ["PATH_INFO"] # /cp/prod-home
         
-        # on enlève le site https://site:port/ (le dernier / n'est pas obligatoire
-        i = self.pi.index("://")
-        p = self.pi[i + 3]
-        try:
-            j = p.index("/")
-            p = p[j + 1:]
-        except:
-            p = ""
-        
-        # on enlève l'extension .a ou .i (quoi que .a n'a pas à être trouvé ici)
+        # on enlève le context-path s'il y en a un
+        l = len(cfg.cp)
+        if l != 0 and p.startswith("/" + cfg.cp + "/"):
+            p = p[l + 2:]       
+        else:
+            p = p[1:]
+        # on enlève l'extension
         self.mode = 1
         i = p.find(".", -1)
         if i != -1:
             ext = p[i + 1:]
-            if ext != "a" and ext != "i":
-                raise AppEx(self.lg, "mode", [ext])
-            p = p[:2]
-            self.mode = 0 if ext == "i" else 2
+            p = p[:i]
+            if ext.startswith("a"):
+                self.mode = 2
+            if ext.startswith("i"):
+                self.mode = 0      
         
-        # on enlève le context-path s'il y en a un
-        if cfg.cpui is not None and p.startswith(cfg.cpui):
-            p = p[len(cfg.cpui):]
+        if p == "sw.js":
+            self.type = 1
+            return
         
-        # on enlève la build
-        i = p.find("_", -1)
-        b = ""
-        if i != -1:
-            b = p[i + 1:]
-            
-            if ext != "a" and ext != "i":
-                raise AppEx(self.lg, "mode")
-            p = p[:2]
-            self.mode = 0 if ext == "i" else 2
+        if p.startswith("z/"):
+            self.type = 2
+            self.org = p[2:]
+            return
         
-        # p : "" raccourci complet
-        # p org
-            
-    def splitBuild(self, b):
-        i = b.find(".")
-        if i == -1:
-            raise AppEx(self.lg, "build", [b])
-        majeur = 0
-        try:
-            majeur = int(b[:i])
-            bx = b[i + 1:]
-            if majeur != self.cfg.inb:
-                raise AppEx(self.lg, "majeur", [str(cfg.inb), str(majeur)])
-            i = bx.find(".")
+        self.type = 3
+        orgHome = p
+        if len(p) == 0:
+            orgHome = homeShortcut()
+        else:
+            i = orgHome.find("-")
             if i == -1:
-                try:
-                    mineur = int(b[i + 1:])
-                    if mineur not in self.cfg.uib:
-                        raise AppEx(self.lg, "mineur", [cfg.deployUib, b])
-                    return [majeur, mineur, 0]
-                except:
-                    raise AppEx(self.lg, "build", [b])
+                x = homeShortcut(orgHome)
+                orgHome = x if x is not None else orgHome + "-index"
+        i = orgHome.find("-")
+        self.org = orgHome[:i]
+        self.home = orgHome[i+1:]
+        
+        qs = environ["QUERY_STRING"]
+        self.lang = defLang()
+        if len(qs) != 0:
+            i = qs.find("lang=")
+            if i != -1:
+                j = qs.find("&", i + 5)
+                self.lang = qs[i + 5:j] if j != -1 else qs[i + 5:]
             else:
-                try:
-                    op = int(b[i + 1:])
-                    if op not in self.cfg.opb:
-                        raise AppEx(self.lg, "op", [cfg.deployOpb, str(op)])
-                    return [majeur, mineur, 0]
-                except:
-                    raise AppEx(self.lg, "build", [b])
-                
-        except:
-            raise AppEx(self.lg, "build", [b])
-                
+                self.lang = defLang()
             
+            self.breq = None
+            i = qs.find("build=")
+            if i != -1:
+                j = qs.index("&", i + 6)
+                x = qs[i + 6:j] if j != -1 else qs[i + 6:]
+                if len(x) != 0:
+                    self.breq = [0, 0, 0]
+                    y = x.split["."]
+                    try:
+                        self.breq[0] = int(y[0]) if len(y) >= 1 else 0
+                        self.breq[1] = int(y[1]) if len(y) >= 2 else 0
+                        self.breq[2] = int(y[2]) if len(y) >= 3 else 0
+                        if self.breq[0] < 1 or self.breq[1] < 0 or self.breq[2] < 0 :
+                            self.breq = None
+                    except:
+                        self.breq = None
+           
+def getSwjs(pyp):
+    global cfg
+    build = str(cfg.inb) + "." + str(cfg.uib[0])
+    rootdir = pyp + "/" + build
+    lx = len(rootdir)
+    cx = "/" + cfg.cp + "/$ui" if (len(cfg.cp) != 0) else "/$ui"
+    lst = []
+    try:
+        for subdir, dirs, files in os.walk(rootdir):
+            for file in files:
+                #print os.path.join(subdir, file)
+                filepath = subdir + os.sep + file
+                lst.append(cx + filepath[lx:])
+        d1 = "const shortcuts = " + json.dumps(cfg.homeShortcuts) + ";\n"
+        d2 = "const build = \"" + build + "\";\nconst cp = \"" + cfg.cp + "\;\nconst lres = [\n"
+        f = open("build/" + build + "/src/sw.js", "r")
+        t = f.read();
+        return d1 + d2 + ",\n".join(lst) + "\n];\n" + t
+    except:
+        raise AppEx(cfg.langs[0], "rb", build, rootdir)
+
+def info(org):
+    global cfg
+    opsite = cfg.opsites.get(org, None)
+    if opsite is None:
+        raise AppEx(cfg.langs[0], "org", org)
+    build = str(cfg.inb) + "." + str(cfg.uib[0])
+    return json.dumps({"uib":build, "opsite":opsite})
+
+def getHome(pyp, url):
+    global cfg
+    build = str(cfg.inb) + "." + str(cfg.uib[0])
+    if url.breq is not None:
+        if url.breq[0] != cfg.inb:
+            raise AppEx(url.lang, "majeur", cfg.inb, url.breq[0])
+        if url.breq[0] == 0:
+            build = str(cfg.inb) + "." + cfg.uib[0]
+        else:
+            if url.breq[1] in cfg.uib:
+                build = str(cfg.inb) + "." +  url.breq[1]
+            else:
+                raise AppEx(url.lang, "mineur", cfg.inb, str(cfg.uib), url.breq[1])
+    rootdir = pyp + "/" + build + "/" + url.home + ".html"
+    try:
+        lst = []
+        done = False
+        cx = "/" if len(cfg.cp) == 0 else "/" + cfg.cp + "/"
+        base = "<base href=\"" + cx + "/" + build + "/\" date-build=\"" + build + "\">\n"
+        with open(rootdir, "r") as ins:
+            for line in ins:
+                if done:
+                    lst.append(line)
+                else:
+                    i = line.find("<base ")
+                    if i != -1:
+                        lst.append(base)
+                        done = True
+                    else:
+                        lst.append(line)
+        return "".join(lst)
+    except:
+        raise AppEx(url.lang, "home", url.home, build, rootdir)
 
 def app(environ, start_response):
+    global cfg
     try:
-        uiUrl = UiUrl(environ)
+        url = Url(environ)
+        if url.type == 1:
+            result = getSwjs(pyp); 
+        elif url.type == 2:
+            result = info(url.org)
+        else:
+            result = getHome(pyp, url)
+        start_response("200 OK", [])
+        return [result]    
+    except Exception as e:
+        start_response("400 Bad Request : " + str(e), [])
+        return (str(e))
         
-        
-    except:
-        
-    result = ExecContext().go(environ, inputData(environ))
-    AL.info(request_uri(environ, include_query=1))
-    AL.info(application_uri(environ))
-    if AL.LEVEL == 0: 
-        AL.info('********** result **********') 
-        AL.info(result.status())
-        AL.info(result.headers())
-        AL.info(result.bytes)
-    start_response(result.status(), result.headers())
-    return [result.bytes]
-
 AL.setInfo()
-httpd = make_server('localhost', 8000, app)
+httpd = make_server('localhost', 80, app)
 AL.warn("Serving on port 8000...")
 httpd.serve_forever()
